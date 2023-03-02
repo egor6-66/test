4359b3bb81f3c8b9c6f5a7749217cf7d4d5b7f4e
Egor <egor@fasthome.io>
2023-03-02 22:29:40 +0300

    dwda

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,17 @@ import {relaunch} from '@tauri-apps/api/process'
 
 function App() {
 
-    // @ts-ignore
-    const RUNNING_IN_TAURI = window.__TAURI__ !== undefined;
-const env = process.env.REACT_APP_ENV_TEST
+    const env = process.env.REACT_APP_ENV_TEST
+    const env2 = process.env.REACT_APP_ENV_TEST_SECRET
     console.log(env)
-    console.log(RUNNING_IN_TAURI)
-    const a = async () => {
-        try {
-            const {shouldUpdate, manifest} = await checkUpdate()
-            console.log(shouldUpdate, manifest)
-            if (shouldUpdate) {
-                // display dialog
-                await installUpdate()
-                // install complete, restart the app
-                await relaunch()
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-a().then()
+    console.log(env2)
+
     return (
         <div className="App">
             <header className="App-header">
-                {env}
-                <button onClick={a}>get version</button>
+               env1 =  {env}
+              <div>  envSecret =  {env2}</div>
+                <button>get version</button>
             </header>
         </div>
     );
